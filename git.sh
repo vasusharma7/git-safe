3693f8355d4aec87127ffd23e243658d228c665c
Vasu Sharma <vasusharma2017@outlook.comwq>
2021-01-12 22:13:49 +0530

    modif: encrypted keys & modif: encrypted keys &

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 git add .
 prev_message=$(git log -1 --pretty=%B)
-echo $message
-new_message="modif: encrypted keys & ${message}"
+echo $prev_message
+new_message="modif: encrypted keys & ${prev_message}"
 git commit -m "$new_message" --no-verify 
